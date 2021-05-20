36ccf88667c09bb112681f050479804c9811ec56
yurimnim <u007x2@gmail.com>
2021-05-21 00:21:59 +0900

    Dropdown filter function is up and running

--- react-course-udemy/01-starting-setup/src/components/Expenses/Expenses.jsx
+++ react-course-udemy/01-starting-setup/src/components/Expenses/Expenses.jsx
@@ -12,20 +12,31 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
-    <Card className='expenses'>
-      <ExpenseFilter
-        selected={filteredYear}
-        onChangeFilter={filterChangeHander}
-      />
-      {props.items.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
+    <div>
+      <Card className='expenses'>
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHander}
         />
-      ))}
-    </Card>
+        {filteredExpenses.length === 0 ? (
+          <p>No expenses found.</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )}
+      </Card>
+    </div>
   );
 };
 
